a8a3345bcb603cd97bea5698c089ab6344b580b1
Lucas Felipe de Ladislau <96193022+felipeeladislau@users.noreply.github.com>
2023-08-30 22:51:25 -0300

    Cine NTCONSULTE

--- src/Desafio3Semana/Modelo/Usuario.java
+++ src/Desafio3Semana/Modelo/Usuario.java
@@ -1,11 +1,11 @@
 package Desafio3Semana.Modelo;
 
 public abstract class Usuario {
-    private String nome;
-    private TipoUsuario tipoUsuario;
-    private int idade;
-    private String usuario;
-    private String senha;
+    private final String nome;
+    private final TipoUsuario tipoUsuario;
+    private final int idade;
+    private final String usuario;
+    private final String senha;
 
     public Usuario(String nome, TipoUsuario tipoUsuario, int idade, String usuario, String senha) {
         this.nome = nome;
@@ -19,41 +19,31 @@ public abstract class Usuario {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+
 
     public TipoUsuario getTipoUsuario() {
         return tipoUsuario;
     }
 
-    public void setTipoUsuario(TipoUsuario tipoUsuario) {
-        this.tipoUsuario = tipoUsuario;
-    }
+
 
     public int getIdade() {
         return idade;
     }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
+
 
     public String getUsuario() {
         return usuario;
     }
 
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
+
 
     public String getSenha() {
         return senha;
     }
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+
 
     // outros métodos comuns a todos os usuários
 }
